4cdadc12886da65116963e9b9bb5d23ffc51a83b
Anh Tran <anh.vainionpaa@gmail.com>
2026-06-06 22:39:20 +0300

    chap2

---
+++ chap3/part11_1.py
@@ -0,0 +1,2 @@
+inputNum = int(input("Give a number: "))
+if (inputNum % 2 == 0): print("The given number was even.")
---
+++ chap3/part12.py
@@ -0,0 +1,8 @@
+selection = "tricycle"
+
+if selection == "money":
+    print("You selected the money!")
+elif selection == "tricycle":
+    print("You selected the tricycle!")
+else:
+    print("Nothing.")
